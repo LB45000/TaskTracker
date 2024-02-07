86f3705afdbc6753ab55dc312345d4596ab24a15
LB45000 <144392042+LB45000@users.noreply.github.com>
2024-02-07 13:22:44 -0800

    Create AddTasks.js

---
+++ src/AddTasks.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function AddTasks({ addTask }) {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(text);
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={(e) => setText(e.target.value)} 
+        placeholder="Add a new task..." />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default AddTasks;
